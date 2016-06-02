124bbe01c1641e30f782fb3c437e6722335034d9
Anamul Hasan <iamanam@ymail.com>
2016-06-03 01:08:23 +0600

    fixed package json

--- imports/ui/layout/common/header/header_helper.js
+++ imports/ui/layout/common/header/header_helper.js
@@ -13,8 +13,6 @@ if (Meteor.isClient) {
 
     Template.header_head.events({
         "click #js-berger-menu": function(e) {
-
-
             e.preventDefault();
             $(e.currentTarget).toggleClass("open");
         }
